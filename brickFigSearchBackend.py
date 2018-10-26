0a895688a3d69d7ec84dc9942f9b23c7f5733052
John Diffor <jwdiffor@gmail.com>
2018-10-26 00:19:55 -0500

    Added port

--- brickFigSearchBackend.py
+++ brickFigSearchBackend.py
@@ -116,4 +116,5 @@ def get_search_results_endpoint(query):
 
 # running web app in local machine
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=5000)
+	port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
